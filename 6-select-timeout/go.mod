df2157d352ac6341da7ede304f4b6c2148de7d03
radoslav.boychev <radoslav.boychev@vegait.rs>
2023-02-28 13:03:13 +0100

    6-select-timeout

---
+++ 6-select-timeout/go.mod
@@ -0,0 +1,3 @@
+module github.com/radoslavboychev/go-concurrency-patterns/6-select-timeout
+
+go 1.20
---
+++ 6-select-timeout/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func boring(msg string) <-chan string {
+	c := make(chan string)
+
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1500)) * time.Millisecond)
+		}
+	}()
+	return c
+}
+
+// runs the goroutine until the timeout is hit
+func main() {
+
+	c := boring("joe")
+
+	timeout := time.After(5 * time.Second)
+	for {
+		select {
+		case s := <-c:
+			fmt.Println(s)
+		case <-timeout:
+			fmt.Println("You talk too much")
+			return
+		}
+	}
+}
